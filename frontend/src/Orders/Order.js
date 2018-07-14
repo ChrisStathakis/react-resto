cfc8320491e4ea57a38bbd576d131b0610264783
Zefarak <lirageika@hotmail.gr>
2018-07-14 04:42:36 +0300

    f july

--- frontend/src/Orders/Order.js
+++ frontend/src/Orders/Order.js
@@ -4,5 +4,36 @@ import 'whatwg-fetch';
 
 class Order extends React.Component {
     
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: null,
+            order: null,
+            table: null,
+            order_items: null,
+            doneLoading: null
+        }
+    }
+
+    loadProducts(){
+        const endpoint = '/api/products/';
+        const thisComp = this;
+        const lookupOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            credentials: 'include'
+        }
+
+        fetch(endpoint, lookupOptions)
+        .then(function(response){
+            return response.json()
+        }).then(function(responseData){
+            thisComp.setState({
+                products: responseData
+            })
+        })
+    }
 }
 
