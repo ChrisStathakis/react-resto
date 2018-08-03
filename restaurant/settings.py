7b82b3a44e4a8f197468551b4e636d326c8ba6c9
Zefarak <lirageika@hotmail.gr>
2018-08-03 16:46:02 +0300

    added start

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z-14t(*3wwp1t-h$bci^oeacj6v4=6^=(oppfd=tx$rsb)yi$)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
